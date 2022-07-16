43252da06e5ba483d5a275f27936627b58901f40
Andrew Lord <andrew@lordcodes.com>
2022-07-16 17:35:55 +0100

    Add SwiftLint using Tuist

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -3,5 +3,6 @@ import ProjectDescription
 let config = Config(
     plugins: [
         .git(url: "https://github.com/lordcodes/swiftformat-tuist", tag: "v0.3.0"),
+        .git(url: "https://github.com/lordcodes/swiftlint-tuist", tag: "v0.1.0"),
     ]
 )
